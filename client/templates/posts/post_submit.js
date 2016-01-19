08df23431ce2778cd56ae9c4495f78289ea057cf
Yoonge <7884008@qq.com>
2016-01-19 19:14:39 +0800

    Day Two.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+    Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -7,13 +20,18 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        var errors = validatePost(post);
+        if (errors.title || errors.url) {
+            return Session.set('postSubmitErrors', errors);
+        }
+
         Meteor.call('postInsert', post, function(error, result) {
 
             if (error)
-                return alert(error.reason);
+                return throwError(error.reason);
 
             if (result.postExists)
-                alert('This link has already been posted（该链接已存在）');
+                throwError('This link has already been posted.');
 
             Router.go('postPage', {
                 _id: result._id
