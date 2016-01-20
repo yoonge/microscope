7572a8e8f4ad00bdbea55f3d030d21892a471622
Yoonge <7884008@qq.com>
2016-01-20 19:17:21 +0800

    Day Three.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tips
+
+1.  Meteor 中不能使用传统分页；
+
+2.  **more**
+
+3.  **To be continued.**
+    <pre><code>fs.writeFile(listFile, listFileContent, function(e) {
+    </code></pre>
+    修改 **fs.writeFile** 为 **fs.appendFile** 可连续扫描多个目录，结果均写入 **list.html**。
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,7 +1,40 @@
-Template.postsList.helpers({
-    posts: function() {
-        return Posts.find({}, {
-            sort: {submitted: -1}
-        });
+Template.postsList.onRendered(function() {
+    this.find('.wrapper')._uihooks = {
+        insertElement: function(node, next) {
+            $(node).hide().insertBefore(next).fadeIn();
+        },
+        moveElement: function(node, next) {
+            var $node = $(node),
+                $next = $(next);
+            var oldTop = $node.offset().top;
+            var height = $node.outerHeight(true);
+
+            var $inBetween = $next.nextUntil(node);
+            if ($inBetween.length === 0) {
+                $inBetween = $node.nextUntil(next);
+            }
+            $node.insertBefore(next);
+
+            var newTop = $node.offset().top;
+            $node.removeClass('animate').css('top', oldTop - newTop);
+            $inBetween.removeClass('animate').css('top', oldTop < newTop ? height : -1 * height);
+
+            $node.offset();
+            $node.addClass('animate').css('top', 0);
+            $inBetween.addClass('animate').css('top', 0);
+        },
+        removeElement: function(node) {
+            $(node).fadeOut(function() {
+                $(this).remove();
+            });
+        }
     }
 });
+
+// Template.postsList.helpers({
+//     posts: function() {
+//         return Posts.find({}, {
+//             sort: {submitted: -1}
+//         });
+//     }
+// });
